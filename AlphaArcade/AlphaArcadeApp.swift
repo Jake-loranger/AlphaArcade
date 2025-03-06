ffff9173224741f3668a45fc545cded87d8fe526
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2025-03-06 17:43:47 -0500

    Initial Commit

---
+++ AlphaArcade/AlphaArcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaArcadeApp.swift
+//  AlphaArcade
+//
+//  Created by Jacob  Loranger on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AlphaArcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
